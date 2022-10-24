965089a0629fbc205e608e97ea61ea45b49f8aac
Thordis Kara Valsdottir <thor9706@stud.kea.dk>
2022-10-24 16:48:25 +0200

    splide & contact

--- src/main.js
+++ src/main.js
@@ -74,4 +74,3 @@ function carousel() {
 }
 
 carousel();
-
---
+++ src/splide.js
@@ -0,0 +1,7 @@
+// SPLIDE
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#image-carousel', {
+          cover      : true,
+          heightRatio: 0.55,
+    } ).mount();
+  } );
